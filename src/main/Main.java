8823581905358776258dc5e2970832036139b780
KyIe <kpeavot@hotmail.com>
2021-03-24 13:30:25 +0000

    Created real word checker using top 10000 common words file

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,37 @@
 package main;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
 
   public static void main(String[] args) {
 
 
   }
+
+  public void caesarCypher(String cypherText) {
+
+
+    
+  }
+
+  /*
+    Checks if given string exists in top 10000 most common words
+    String must be single word
+   */
+  public static boolean isStringRealWord(String stringToCheck) {
+    try {
+      File commonWords = new File("./src/resources/statisticalHelpers/commonWords.txt");
+      Scanner myReader = new Scanner(commonWords);
+      return myReader.findAll(stringToCheck.trim())
+        .anyMatch(matchResult -> true);
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+      return false;
+    }
+  }
+
 
 }
